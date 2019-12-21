d2b19ef3bc6a1cb03dc565e201442ed1f468af10
soojung <blessme333@gmail.com>
2019-12-21 17:03:47 +0900

    XML ID 부여

---
+++ app/src/main/java/com/soojung/a2019122104_pizzastorelistapp/PizzaStoreDetailActivity.kt
@@ -0,0 +1,31 @@
+package com.soojung.a2019122104_pizzastorelistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PizzaStoreDetailActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pizza_store_detail)
+        setupEvents()
+        setValues()
+
+
+    }
+
+
+
+
+    override fun setupEvents() {
+
+
+    }
+
+    override fun setValues() {
+
+
+    }
+
+}
