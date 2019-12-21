1f75c66fb8969070364de6b51e4f25ca761a1326
soojung <blessme333@gmail.com>
2019-12-21 15:33:16 +0900

    Initial commit

---
+++ app/src/main/java/com/soojung/a2019122104_pizzastorelistapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.soojung.a2019122104_pizzastorelistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
