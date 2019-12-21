f8dfcd469166786d342546d4388c5883326001aa
soojung <blessme333@gmail.com>
2019-12-21 15:35:34 +0900

    피자가게의 근거 데이터 클래스 설정

---
+++ app/src/main/java/com/soojung/a2019122104_pizzastorelistapp/datas/PizzaStore.kt
@@ -0,0 +1,9 @@
+package com.soojung.a2019122104_pizzastorelistapp.datas
+
+class PizzaStore (storeName:String, phoneNum:String, logoUrl:String){
+
+    var storeName = storeName
+    var phoneNum = phoneNum
+    var logoUrl = logoUrl
+
+}
